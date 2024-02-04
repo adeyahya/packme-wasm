dc7b21588152cd063510e67b0b7f14806085251e
Ade Yahya <adeyahyaprasetyo@gmail.com>
2024-02-04 13:24:31 +0700

    feat: pack layer

--- src/packing/algo.rs
+++ src/packing/algo.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use super::{Container, Item};
 
 #[derive(Default)]
@@ -12,9 +14,18 @@ struct Layer {
     pub layer_eval: f64,
 }
 
+#[derive(Default, Clone)]
+struct Scrappad {
+    cumx: f64,
+    cumz: f64,
+    pre: Option<Box<Scrappad>>,
+    pos: Option<Box<Scrappad>>,
+}
+
 pub struct EbAfit<'a> {
     pub container: &'a Container,
     item_list: Vec<Item>,
+    item_packing_status: HashMap<usize, bool>,
     layer_list: Vec<Layer>,
     orientation_variant: OrientationVariant<'a>,
     orientation: Vector3,
@@ -23,8 +34,21 @@ pub struct EbAfit<'a> {
     pub total_box_vol: f64,
     // current box index that being evaluated
     bn: usize,
-
     same: bool,
+    iteration_count: usize,
+    packed_vol: f64,
+    packedy: f64,
+    is_packing: bool,
+    layer_tickness: f64,
+    remainpy: f64,
+    remainpz: f64,
+    packed_num_box: usize,
+    is_quit: bool,
+    scrappad: Scrappad,
+    scrapfirst: Option<Box<Scrappad>>,
+    scrapmemb: Option<Box<Scrappad>>,
+    smallestz: Option<Box<Scrappad>>,
+    trash: Option<Box<Scrappad>>,
 }
 
 // public trait
@@ -56,6 +80,7 @@ impl<'a> EbAfit<'a> {
         Self {
             container,
             item_list: computed_item_list,
+            item_packing_status: HashMap::new(),
             layer_list: Vec::new(),
             orientation_variant,
             orientation,
@@ -63,6 +88,20 @@ impl<'a> EbAfit<'a> {
             same: false,
             temp: 0.0,
             bn: 0,
+            iteration_count: 0,
+            packed_vol: 0.0,
+            packedy: 0.0,
+            is_packing: false,
+            layer_tickness: 0.0,
+            remainpy: 0.0,
+            remainpz: 0.0,
+            packed_num_box: 0,
+            is_quit: false,
+            scrappad: Scrappad::default(),
+            scrapfirst: Some(Box::new(Scrappad::default())),
+            scrapmemb: None,
+            smallestz: None,
+            trash: None,
         }
     }
 
@@ -149,6 +188,66 @@ impl<'a> EbAfit<'a> {
             });
         }
     }
+
+    //********************************************************
+    // FINDS THE FIRST TO BE PACKED GAP IN THE LAYER EDGE
+    //********************************************************
+    fn find_smallest_z(&mut self) {
+        if let Some(scrapfirst) = self.scrapfirst.as_mut() {
+            self.scrapmemb = Some(scrapfirst.clone());
+            self.smallestz = Some(scrapfirst.clone());
+
+            let smallestz = self.smallestz.as_mut().unwrap();
+            let scrapmemb = self.scrapmemb.as_mut().unwrap();
+            while let Some(pos) = scrapmemb.pos.as_deref() {
+                if pos.cumz < smallestz.cumz {
+                    *smallestz = Box::new(pos.clone());
+                }
+                *scrapmemb = Box::new(pos.clone());
+            }
+        }
+    }
+
+    //**********************************************************************
+    // PACKS THE BOXES FOUND AND ARRANGES ALL VARIABLES AND
+    // RECORDS PROPERLY
+    //**********************************************************************
+    fn pack_layer(&mut self) -> bool {
+        let lenx = 0.0;
+        let lenz = 0.0;
+        let lpz = 0.0;
+        if self.layer_tickness == 0.0 {
+            self.is_packing = false;
+            return false;
+        };
+
+        if let Some(scrapfirst) = self.scrapfirst.as_mut() {
+            scrapfirst.cumx = self.orientation.x;
+            scrapfirst.cumz = 0.0;
+        }
+
+        while self.is_quit == false {
+            self.find_smallest_z();
+            if let Some(smallestz) = self.smallestz.as_deref() {
+                match (smallestz.pre.as_deref(), smallestz.pos.as_deref()) {
+                    //*** SITUATION-1: NO BOXES ON THE RIGHT AND LEFT SIDES ***
+                    (None, None) => {}
+                    //*** SITUATION-2: NO BOXES ON THE LEFT SIDE ***
+                    (None, _) => {}
+                    //*** SITUATION-3: NO BOXES ON THE RIGHT SIDE ***
+                    (_, None) => {}
+                    (Some(pre), Some(pos)) => {
+                        //*** SITUATION-4: THERE ARE BOXES ON BOTH OF THE SIDES ***
+
+                        //*** SUBSITUATION-4A: SIDES ARE EQUAL TO EACH OTHER ***
+                        if pre.cumz == pos.cumz {}
+                    }
+                }
+            }
+        }
+
+        false
+    }
 }
 
 // implement iterator for the computation loop
@@ -163,9 +262,6 @@ impl<'a> Iterator for EbAfit<'a> {
             self.bn += 1;
             Some(())
         } else {
-            // eof loop for current orientation
-            // proceed to next orientation if available
-
             // sets the evaluation of the first layer in the list to -1
             if let Some(layer) = self.layer_list.get_mut(0) {
                 layer.layer_eval = -1.0;
@@ -182,6 +278,23 @@ impl<'a> Iterator for EbAfit<'a> {
                         .unwrap_or(std::cmp::Ordering::Equal)
                 }
             });
+
+            self.item_packing_status = HashMap::new();
+            let mut orientation_peekable = self.orientation_variant.clone().peekable();
+            let orientation = orientation_peekable.peek().unwrap();
+            for layer in self.layer_list.iter() {
+                self.iteration_count += 1;
+                self.packed_vol = 0.0;
+                self.packedy = 0.0;
+                self.is_packing = true;
+                self.layer_tickness = layer.layer_dim;
+                self.remainpy = orientation.y;
+                self.remainpz = orientation.z;
+                self.packed_num_box = 0 as usize;
+            }
+
+            // eof loop for current orientation
+            // proceed to next orientation if available
             if let Some(orientation) = self.orientation_variant.next() {
                 self.bn = 0;
                 self.orientation = orientation;
@@ -195,6 +308,7 @@ impl<'a> Iterator for EbAfit<'a> {
 
 // iterator that represent 6 possible different orientation
 // of the container
+#[derive(Clone)]
 struct OrientationVariant<'a> {
     current_variant: usize,
     container: &'a Container,
